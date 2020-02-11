02cba2183f590b2a72e863f0aff239eb18fdb862
harootyoon <harutyuna97@gmail.com>
2020-02-11 22:07:07 +0400

    eerrr

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import authReducer from './authReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore';
-import { firebaseReducer } from 'react-redux-firebase'
+import { firebaseReducer } from 'react-redux-firebase';
+import { quizReducer } from './quizReducer'
 
 const rootReducer = combineReducers({
+  quiz: quizReducer,
   auth: authReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer
